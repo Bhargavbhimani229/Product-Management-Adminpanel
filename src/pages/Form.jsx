1b73b6e75af9264c8d61e3405319de7d3636f196
Bhargavbhimani229 <bhargavbhimani229@gmail.com>
2025-05-26 17:11:40 +0530

    SetUp-done

---
+++ src/pages/Form.jsx
@@ -0,0 +1,170 @@
+import React from "react";
+import Aside from "../components/Aside";
+import Header from "../components/Header";
+
+const Form = () => {
+  return (
+    <>
+      <div className="wrapper">
+        {/* Sidebar */}
+        <Aside />
+        {/* End Sidebar */}
+        <div className="main-panel">
+          <div className="main-header">
+            <div className="main-header-logo">
+              {/* Logo Header */}
+              <div className="logo-header" data-background-color="dark">
+                <a href="../index.html" className="logo">
+                  <img
+                    src="../assets/img/kaiadmin/logo_light.svg"
+                    alt="navbar brand"
+                    className="navbar-brand"
+                  />
+                </a>
+                <div className="nav-toggle">
+                  <button className="btn btn-toggle toggle-sidebar">
+                    <i className="gg-menu-right" />
+                  </button>
+                  <button className="btn btn-toggle sidenav-toggler">
+                    <i className="gg-menu-left" />
+                  </button>
+                </div>
+                <button className="topbar-toggler more">
+                  <i className="gg-more-vertical-alt" />
+                </button>
+              </div>
+              {/* End Logo Header */}
+            </div>
+            {/* Navbar Header */}
+            <Header />
+            {/* End Navbar */}
+          </div>
+          <div className="container">
+            <div className="page-inner">
+              <div className="page-header">
+                <h3 className="fw-bold mb-3">Forms</h3>
+                <ul className="breadcrumbs mb-3">
+                  <li className="nav-home">
+                    <a href="#">
+                      <i className="icon-home" />
+                    </a>
+                  </li>
+                  <li className="separator">
+                    <i className="icon-arrow-right" />
+                  </li>
+                  <li className="nav-item">
+                    <a href="#">Forms</a>
+                  </li>
+                  <li className="separator">
+                    <i className="icon-arrow-right" />
+                  </li>
+                  <li className="nav-item">
+                    <a href="#">Basic Form</a>
+                  </li>
+                </ul>
+              </div>
+              <div className="row">
+                <div className="col-md-12">
+                  <div className="card">
+                    <div className="card-header">
+                      <div className="card-title">Form Elements</div>
+                    </div>
+                    <div className="card-body">
+                      <div className="row">
+                        <div className="col-md-6 col-lg-4">
+                          <div className="form-group">
+                            <label htmlFor="product-name">Product Name</label>
+                            <input
+                              type="text"
+                              className="form-control"
+                              id="product-name"
+                              name="product-name"
+                              placeholder="Enter Product Name"
+                            />
+                          </div>
+                           <div className="form-group">
+                            <label htmlFor="stock">Product Stock</label>
+                            <input
+                              type="text"
+                              className="form-control"
+                              id="stock"
+                              name="stock"
+                              placeholder="Enter Product stock"
+                            />
+                          </div>
+                           <div className="form-group">
+                            <label htmlFor="image">Image</label>
+                            <input
+                              type="file"
+                              className="form-control"
+                              id="image"
+                              name="image"
+                              placeholder="Enter Product Name"
+                            />
+                          </div>
+                           <div className="form-group">
+                            <label htmlFor="product-price">Product Price</label>
+                            <input
+                              type="text"
+                              className="form-control"
+                              id="price"
+                              name="price"
+                              placeholder="Enter Product price"
+                            />
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                    <div className="card-action">
+                      <button className="btn btn-success">Submit</button>
+                      <button className="btn btn-danger">Cancel</button>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <footer className="footer">
+            <div className="container-fluid d-flex justify-content-between">
+              <nav className="pull-left">
+                <ul className="nav">
+                  <li className="nav-item">
+                    <a className="nav-link" href="http://www.themekita.com">
+                      ThemeKita
+                    </a>
+                  </li>
+                  <li className="nav-item">
+                    <a className="nav-link" href="#">
+                      {" "}
+                      Help{" "}
+                    </a>
+                  </li>
+                  <li className="nav-item">
+                    <a className="nav-link" href="#">
+                      {" "}
+                      Licenses{" "}
+                    </a>
+                  </li>
+                </ul>
+              </nav>
+              <div className="copyright">
+                2024, made with <i className="fa fa-heart heart text-danger" />{" "}
+                by
+                <a href="http://www.themekita.com">ThemeKita</a>
+              </div>
+              <div>
+                Distributed by
+                <a target="_blank" href="https://themewagon.com/">
+                  ThemeWagon
+                </a>
+                .
+              </div>
+            </div>
+          </footer>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Form;
